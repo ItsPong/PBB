76bf27692d71e54b0f9818c55fc09317ab31c9d6
M. Shafa N. <142976367+ItsPong@users.noreply.github.com>
2026-06-10 01:40:03 +0700

    Pertemuan_12

---
+++ Pertemuan_12/app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.LaunchedEffect
+import com.example.mvvm.data.local.database.AppDatabase
+import com.example.mvvm.data.repository.UserRepository
+import com.example.mvvm.ui.screen.LoginScreen
+import com.example.mvvm.ui.theme.MVVMTheme
+import com.example.mvvm.viewmodel.LoginViewModel
+import com.example.mvvm.viewmodel.LoginViewModelFactory
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val database = AppDatabase.getDatabase(this)
+        val repository = UserRepository(database.userDao())
+        val factory = LoginViewModelFactory(repository)
+
+        setContent {
+            MVVMTheme {
+                val viewModel: LoginViewModel = viewModel(factory = factory)
+
+                LaunchedEffect(Unit) {
+                    viewModel.insertDummyUser()
+                }
+
+                LoginScreen(viewModel)
+            }
+        }
+    }
+}
